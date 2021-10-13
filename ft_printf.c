7706b0dbec8ebec220824b1b9353881f77fc43a7
pirichar <pirichar@student.42quebec.com>
2021-10-13 15:12:28 -0400

    first commit

---
+++ ft_printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/*program name libftprintf.a
+Turn in files :*.c, */*.c, *.h, */*.h, Makefile
+Makefile :all, clean, fclean, re, bonus
+External functs: malloc, free, write, va_start, va_arg, va_copy,va_end
+Libft authorized yes
+Description Write a library that contains ft_printf, a function
+that will mimic the real printf*/
+
+int ft_printf(const char *format, ...()
+{
+    int sum;
+
+    sum = 0;
+    va_list argptr;
+
+    va_start(argptr, num)
+
+
+}
