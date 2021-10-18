842f20aa6498f8eedf1f6cd266911b9fe595bfe2
pirichar <pirichar@student.42quebec.com>
2021-10-18 10:20:38 -0400

    cleaning the fuckin file

--- ft_printf.c
+++ ft_printf.c
@@ -2,53 +2,27 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-/*program name libftprintf.a
-Turn in files :*.c, **.c, *.h, **.h, Makefile
-Makefile :all, clean, fclean, re, bonus
-External functs: 
-malloc, 
-free, 
-write,
-va_start,
-va_arg,
-va_copy,va_end
-Libft authorized yes
-Description Write a library that contains ft_printf, a function
-that will mimic the real printf
-A small description of the required conversion:
-• %c print a single character.
-• %s print a string of characters.
-• %p The void * pointer argument is printed in hexadecimal. • %d print a decimal (base 10) number
-• %i print an integer in base 10.
-• %u print an unsigned decimal (base 10) number.
-• %x print a number in hexadecimal (base 16), with lowercase. • %X print a number in hexadecimal (base 16), with uppercase. • %% print a percent sign.
-*/
-
-int ft_printf(const char *format, ...)
+int	ft_printf(const char*format, ...)
 {
-    int sum;
-    int i;
-    char *output;
+	int	sum;
+	int i;
+	char *output;
 
-    sum = 0;
-    i = 0;
-    va_list argptr;
+	sum = 0;
+	i = 0;
+	va_list argptr;
 
-    va_start(argptr, format);
-    while(format)
-    {
-	    output = va_arg(argptr, char *);
-	    output++;
-    }
-    va_end(argptr);
-    return (sum);
-
+	va_start(argptr, format);
+	while (format)
+	{
+		output = va_arg(argptr, char *)
+		output ++;
+	}
+	va_end(argptr);
+	return (sum);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-
 	return (0);
-
 }
-
