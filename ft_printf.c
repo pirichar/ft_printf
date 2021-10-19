111c530b41dfae1c21007a495f4429d95db6e927
pirichar <pirichar@student.42quebec.com>
2021-10-19 12:19:59 -0400

    au tout debut ca marche avec un string entre guillement

--- ft_printf.c
+++ ft_printf.c
@@ -2,27 +2,59 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-int	ft_printf(const char*format, ...)
+/*External functions permitted :
+ * malloc, free, write, va_start, va_arg, va_copy, va_end
+ *
+ * Conversions to manage :
+ * %c - To print a single Char
+ * %s - To print a whole string
+ * %p - The void * pointer printed in hexa
+ * %d - to print a decimal (base 10) number
+ * %i - to print an integer in base 10
+ * %u - To print an unsigned decimal (base 10) number
+ * %x - to print a number in hexadecimal (base 16), with lowercase
+ * %X - to print a number in hexadecimal (base 16, with uppercase
+ * %% - to print a percent sign*/
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	ft_putstring_int(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_printf(const char *format, ...)
 {
 	int	sum;
-	int i;
-	char *output;
+	va_list ap;
 
 	sum = 0;
-	i = 0;
-	va_list argptr;
+	va_start(ap, format);
+	//in case its just a string 
+	sum = ft_putstring_int((char *)format);
+	format++;
 
-	va_start(argptr, format);
-	while (format)
-	{
-		output = va_arg(argptr, char *)
-		output ++;
-	}
-	va_end(argptr);
+	//
+	va_end(ap);
+	printf("Sum in my function is = %i\n", sum);
 	return (sum);
 }
 
-int main(int argc, char * argv[])
+
+
+int main(void)
 {
-	return (0);
+	
+	ft_printf("Allo les amis\n");
 }
